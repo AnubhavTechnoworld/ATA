fd911cde178db2c32c564bdb9015eadf2e681fd0
Anubhav <anubhavpathak4@gmail.com>
2018-09-10 11:46:44 +0530

    Add files via upload

---
+++ Automatic/src/com/training/ata/bean/CredentialsBean.java
@@ -0,0 +1,38 @@
+package com.training.ata.bean;
+
+public class CredentialsBean 
+{
+		private String userID;
+		private String password;
+		private String usertype;
+		private int loginStatus;
+		
+		
+		public String getUserID() {
+			return userID;
+		}
+		public void setUserID(String userID) {
+			this.userID = userID;
+		}
+		public String getPassword() {
+			return password;
+		}
+		public void setPassword(String password) {
+			this.password = password;
+		}
+		public String getUsertype() {
+			return usertype;
+		}
+		public void setUsertype(String usertype) {
+			this.usertype = usertype;
+		}
+		public int getLoginStatus() {
+			return loginStatus;
+		}
+		public void setLoginStatus(int loginStatus) {
+			this.loginStatus = loginStatus;
+		}
+		
+		
+
+}
